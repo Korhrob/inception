06404a185308c4ab07a8aea9e93777273a54bae1
rkorhone <korhrob@gmail.com>
2025-04-14 11:46:55 +0300

    changed to ssl connection, installing wordpress via entrypoint and checking for wordpress container health for nginx container

--- srcs/requirements/nginx/tools/nginx-entrypoint.sh
+++ srcs/requirements/nginx/tools/nginx-entrypoint.sh
@@ -2,5 +2,10 @@
 
 set -e
 
+echo "Waiting for WordPress to be ready..."
+until curl -s -f http://localhost/wp-login.php > /dev/null; do
+    sleep 1
+done
+
 echo "Starting NGINX..."
 nginx -g "daemon off;"
--- srcs/requirements/wordpress/tools/wp-entrypoint.sh
+++ srcs/requirements/wordpress/tools/wp-entrypoint.sh
@@ -2,6 +2,34 @@
 
 set -e
 
+until mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do
+    sleep 1
+done
+
+if ! wp core is-installed --allow-root; then
+    echo "Installing WordPress..."
+
+	if echo "$WORDPRESS_ADMIN_USER" | grep -i 'admin'; then
+		echo "Error: Admin username cannot contain 'admin' or 'administrator'"
+		exit 1
+	fi
+
+    wp core install \
+        --url="$WORDPRESS_URL" \
+        --title="$WORDPRESS_TITLE" \
+        --admin_user="$WORDPRESS_ADMIN_USER" \
+        --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+        --admin_email="$WORDPRESS_ADMIN_EMAIL" \
+        --skip-email \
+        --allow-root
+
+    wp user create "$WP_SECOND_USER" "$WP_SECOND_USER_EMAIL" \
+        --user_pass="$WP_SECOND_USER_PASSWORD" \
+        --role=editor \
+        --allow-root
+else
+    echo "WordPress is already installed."
+fi
+
 echo "Starting PHP..."
 exec php-fpm7.4 -F
-
